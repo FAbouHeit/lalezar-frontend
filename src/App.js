cf0458ae0c2c463c0c30d86c2c83406ed7d9c41f
louai <louaibaghdadi27@gmail.com>
2024-01-19 16:21:31 +0200

    start card in product

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import './App.css';
-import AppRouter from './Routes/Routes';
+import "./App.css";
+import AppRouter from "./Routes/Routes";
+import {
+  QueryClient,
+  QueryClientProvider,
+  useQuery,
+} from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
     <div className="App">
-      <AppRouter/>
+      <QueryClientProvider client={queryClient}>
+        <AppRouter />
+      </QueryClientProvider>
     </div>
   );
 }
--- src/Pages/Products/Products.js
+++ src/Pages/Products/Products.js
@@ -6,9 +6,81 @@ import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
 import TextField from "@mui/material/TextField";
 import Stack from "@mui/material/Stack";
 import Autocomplete from "@mui/material/Autocomplete";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import { styled } from "@mui/material/styles";
+import Card from "@mui/material/Card";
+import CardHeader from "@mui/material/CardHeader";
+import CardMedia from "@mui/material/CardMedia";
+import CardContent from "@mui/material/CardContent";
+import CardActions from "@mui/material/CardActions";
+import Collapse from "@mui/material/Collapse";
+import Avatar from "@mui/material/Avatar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import { red } from "@mui/material/colors";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import ShareIcon from "@mui/icons-material/Share";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+
+const ExpandMore = styled((props) => {
+  const { expand, ...other } = props;
+  return <IconButton {...other} />;
+})(({ theme, expand }) => ({
+  transform: !expand ? "rotate(0deg)" : "rotate(180deg)",
+  marginLeft: "auto",
+  transition: theme.transitions.create("transform", {
+    duration: theme.transitions.duration.shortest,
+  }),
+}));
 
 const Products = () => {
+  const [expanded, setExpanded] = React.useState(false);
+
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
   const [sidePanelWidth, setSidePanelWidth] = useState(400);
+  const { isPending, error, data } = useQuery({
+    queryKey: ["repoData"],
+    queryFn: () =>
+      axios
+        .get(`${process.env.REACT_APP_BACKEND_ENDPOINT}categories`)
+        .then((res) => {
+          console.log(res.data);
+          return res.data;
+        }),
+  });
+
+  // console.log(data)
+
+  const updateSideBar = () => {
+    if (window.innerWidth > 960) {
+      setSidePanelWidth(400);
+    } else {
+      setSidePanelWidth(0);
+    }
+  };
+
+  useEffect(() => {
+    updateSideBar();
+    window.addEventListener("resize", updateSideBar);
+    return () => {
+      window.removeEventListener("resize", updateSideBar);
+    };
+  }, []);
+
+  if (isPending) return "Loading";
+  if (error) return "An error has occurred: " + error.message;
+
+  const openNav = () => {
+    setSidePanelWidth(400);
+  };
+
+  const closeNav = () => {
+    setSidePanelWidth(0);
+  };
 
   const top100Films = [
     { title: "The Shawshank Redemption", year: 1994 },
@@ -19,31 +91,6 @@ const Products = () => {
     { title: "Schindler's List", year: 1993 },
     { title: "Pulp Fiction", year: 1994 },
   ];
-
-  const openNav = () => {
-    setSidePanelWidth(400);
-  };
-
-  const closeNav = () => {
-    setSidePanelWidth(0);
-  };
-
-  useEffect(() => {
-    function updateSideBar() {
-      if (window.innerWidth > 960) {
-        setSidePanelWidth(400);
-      } else {
-        setSidePanelWidth(0);
-      }
-    }
-
-    window.addEventListener("resize", updateSideBar);
-    updateSideBar();
-
-    return () => {
-      window.removeEventListener("resize", updateSideBar);
-    };
-  }, []);
 
   return (
     <div style={{ display: "flex" }} className={StyleProducts.big}>
@@ -64,7 +111,7 @@ const Products = () => {
           <section className={StyleProducts.searchArticle}>
             <article>
               <h3>Brand</h3>
-              <Stack  className={StyleProducts.stack}>
+              <Stack className={StyleProducts.stack}>
                 <Autocomplete
                   freeSolo
                   id="free-solo-2-demo"
@@ -89,46 +136,18 @@ const Products = () => {
             <article>
               <h3>Categories</h3>
               <div className={StyleProducts.checkBoxContainer}>
-                <div className={StyleProducts.checkBoxLine}>
-                  <input
-                    type="checkbox"
-                    id="Eastern&Western"
-                    name="Eastern&Western"
-                    value="Eastern&Western"
-                    className={StyleProducts.customCheckbox}
-                  />
-                  <label htmlFor="Eastern&Western"> Eastern & Western</label>
-                </div>
-                <div className={StyleProducts.checkBoxLine}>
-                  <input
-                    type="checkbox"
-                    id="Eastern&Western"
-                    name="Eastern&Western"
-                    value="Eastern&Western"
-                    className={StyleProducts.customCheckbox}
-                  />
-                  <label htmlFor="Eastern&Western"> Eastern & Western</label>
-                </div>
-                <div className={StyleProducts.checkBoxLine}>
-                  <input
-                    type="checkbox"
-                    id="Eastern&Western"
-                    name="Eastern&Western"
-                    value="Eastern&Western"
-                    className={StyleProducts.customCheckbox}
-                  />
-                  <label htmlFor="Eastern&Western"> Eastern & Western</label>
-                </div>
-                <div className={StyleProducts.checkBoxLine}>
-                  <input
-                    type="checkbox"
-                    id="Eastern&Western"
-                    name="Eastern&Western"
-                    value="Eastern&Western"
-                    className={StyleProducts.customCheckbox}
-                  />
-                  <label htmlFor="Eastern&Western"> Eastern & Western</label>
-                </div>
+                {data.map((category) => (
+                  <div key={category.id} className={StyleProducts.checkBoxLine}>
+                    <input
+                      type="checkbox"
+                      id={category.name}
+                      name={category.name}
+                      value={category.name}
+                      className={StyleProducts.customCheckbox}
+                    />
+                    <label htmlFor={category.name}>{category.name}</label>
+                  </div>
+                ))}
               </div>
             </article>
           </section>
@@ -158,8 +177,84 @@ const Products = () => {
       )}
 
       <div className={StyleProducts.content}>
-        <h2>Collapsed Sidepanel</h2>
-        <p>Content...</p>
+        <Card sx={{ maxWidth: 345 }}>
+          <CardHeader
+            avatar={
+              <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
+                R
+              </Avatar>
+            }
+            action={
+              <IconButton aria-label="settings">
+                <MoreVertIcon />
+              </IconButton>
+            }
+            title="Shrimp and Chorizo Paella"
+            subheader="September 14, 2016"
+          />
+          <CardMedia
+            component="img"
+            height="194"
+            image="/static/images/cards/paella.jpg"
+            alt="Paella dish"
+          />
+          <CardContent>
+            <Typography variant="body2" color="text.secondary">
+              This impressive paella is a perfect party dish and a fun meal to
+              cook together with your guests. Add 1 cup of frozen peas along
+              with the mussels, if you like.
+            </Typography>
+          </CardContent>
+          <CardActions disableSpacing>
+            <IconButton aria-label="add to favorites">
+              <FavoriteIcon />
+            </IconButton>
+            <IconButton aria-label="share">
+              <ShareIcon />
+            </IconButton>
+            <ExpandMore
+              expand={expanded}
+              onClick={handleExpandClick}
+              aria-expanded={expanded}
+              aria-label="show more"
+            >
+              <ExpandMoreIcon />
+            </ExpandMore>
+          </CardActions>
+          <Collapse in={expanded} timeout="auto" unmountOnExit>
+            <CardContent>
+              <Typography paragraph>Method:</Typography>
+              <Typography paragraph>
+                Heat 1/2 cup of the broth in a pot until simmering, add saffron
+                and set aside for 10 minutes.
+              </Typography>
+              <Typography paragraph>
+                Heat oil in a (14- to 16-inch) paella pan or a large, deep
+                skillet over medium-high heat. Add chicken, shrimp and chorizo,
+                and cook, stirring occasionally until lightly browned, 6 to 8
+                minutes. Transfer shrimp to a large plate and set aside, leaving
+                chicken and chorizo in the pan. Add pimentón, bay leaves,
+                garlic, tomatoes, onion, salt and pepper, and cook, stirring
+                often until thickened and fragrant, about 10 minutes. Add
+                saffron broth and remaining 4 1/2 cups chicken broth; bring to a
+                boil.
+              </Typography>
+              <Typography paragraph>
+                Add rice and stir very gently to distribute. Top with artichokes
+                and peppers, and cook without stirring, until most of the liquid
+                is absorbed, 15 to 18 minutes. Reduce heat to medium-low, add
+                reserved shrimp and mussels, tucking them down into the rice,
+                and cook again without stirring, until mussels have opened and
+                rice is just tender, 5 to 7 minutes more. (Discard any mussels
+                that don&apos;t open.)
+              </Typography>
+              <Typography>
+                Set aside off of the heat to let rest for 10 minutes, and then
+                serve.
+              </Typography>
+            </CardContent>
+          </Collapse>
+        </Card>
       </div>
     </div>
   );
