b9992d3f1b87fb62b06df05b69c0c4e5768fbe94
louai <louaibaghdadi27@gmail.com>
2024-01-21 20:35:14 +0200

    serviceWorker

---
+++ public/service-worker.js
@@ -0,0 +1,18 @@
+// public/service-worker.js
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+      caches.open('api-cache').then((cache) => {
+        return cache.match(event.request).then((cachedResponse) => {
+          return (
+            cachedResponse ||
+            fetch(event.request).then((networkResponse) => {
+              cache.put(event.request, networkResponse.clone());
+              return networkResponse;
+            })
+          );
+        });
+      })
+    );
+  });
+  
