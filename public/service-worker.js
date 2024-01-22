9a1738cc4e58c452b272bd3b3b8b0371697b34eb
louai <louaibaghdadi27@gmail.com>
2024-01-22 10:39:18 +0200

    test

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,12 +1,12 @@
 // public/service-worker.js
 
 self.addEventListener('fetch', (event) => {
-    event.respondWith(
-      caches.open('api-cache').then((cache) => {
-        return cache.match(event.request).then((cachedResponse) => {
+    event.respondWith( // to provide a custom response to the browser instead of the default network response
+      caches.open('api-cache').then((cache) => { // open a specific cache name
+        return cache.match(event.request).then((cachedResponse) => { // attempts to find a matching response in the cache or maybe undefined
           return (
             cachedResponse ||
-            fetch(event.request).then((networkResponse) => {
+            fetch(event.request).then((networkResponse) => { // in case of undefined , it fetches the resource from the network
               cache.put(event.request, networkResponse.clone());
               return networkResponse;
             })
