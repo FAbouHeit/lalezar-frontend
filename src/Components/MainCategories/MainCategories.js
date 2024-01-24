883737097a9afdb3b97bfba6d04b7c2fe8765291
WouroudElKhaldi <wouroudelkhaldi@gmail.com>
2024-01-24 13:49:15 +0200

    finish about us, edit on home page for fix styes

--- src/Components/MainCategories/MainCategories.js
+++ src/Components/MainCategories/MainCategories.js
@@ -3,38 +3,38 @@ import Styles from "./MainCategories.module.css";
 import image1 from "../../Assets/category1.jpg";
 import image2 from "../../Assets/category2.jpg";
 import { Button } from "@mui/material";
+import Spices from "../../Assets/Spices.png";
+import Organic from "../../Assets/Organic.png";
 
 const MainCategories = () => {
   const data = [
-    { title: "Most tastier spices ever", img: image1 },
-    { title: "100% Organic Products", img: image2 },
+    { title: "Most tastier spices ever", img: Spices, alt: "Spices" },
+    { title: "100% Organic Products", img: Organic, alt: "Organic Product" },
   ];
   return (
     <article className={Styles.container}>
       {data.map((item, index) => {
-        const divClassName =
-          index === 1 ? Styles.insideRight : Styles.insideLeft;
-        const imgClass = index === 1 ? Styles.imgRight : Styles.imgLeft;
         return (
-          <section className={Styles.main} key={index}>
-            <img src={item.img} alt="img" className={imgClass} />
-            <div className={divClassName}>
-              <p className={Styles.P}>{item.title}</p>
-              <div className={Styles.btn}>
-                <Button
-                  sx={{
-                    fontSize: '0.8rem',
-                    bgcolor: "#C86823",
-                    color: "white",
-                    ":hover": {
-                      bgcolor: "#A0471D",
-                    },
-                  }}
-                >
-                  View more
-                </Button>
-              </div>
-            </div>
+          <section
+            className={`${Styles.section} ${Styles.spin} ${Styles.circle}`}
+            key={index}
+          >
+            <img src={item.img} alt={item.alt} className={Styles.img} />
+            <p className={Styles.p}>{item.title}</p>
+            <span className={Styles.btn}>
+              <Button
+                variant="contained"
+                sx={{
+                  zIndex: 1,
+                  bgcolor: "#C86823",
+                  ":hover": {
+                    bgcolor: "#A0471D",
+                  },
+                }}
+              >
+                View More
+              </Button>
+            </span>
           </section>
         );
       })}
