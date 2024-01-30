3218360fd81d0a1070763b7ac36efa52f4113fed
whathow <marwa@gmail.com>
2024-01-30 15:06:42 +0200

    fix login total price  functionality

--- src/Components/CardItem/CardItem.js
+++ src/Components/CardItem/CardItem.js
@@ -1,21 +1,21 @@
 import React from 'react'
 import styles from "./CardItem.module.css"
 import DeleteOutlineIcon from '@mui/icons-material/DeleteOutline';
-import { useState,useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { IconButton } from '@mui/material';
 import AddIcon from '@mui/icons-material/Add';
 import RemoveIcon from '@mui/icons-material/Remove';
 
-export default function CardItem({id,setTotalPrice, name,quantity,price,color,image,onDelete,totalPrice,calculateTotalPrice}) {
+export default function CardItem({ id, setTotalPrice, name, quantity, price, color, image, onDelete, totalPrice, calculateTotalPrice }) {
   const [count, setCount] = useState(quantity);
   // const [totalPrice, setTotalPrice] = useState(quantity * price);
   const [editQuantity, setEditQuantity] = useState(quantity);
   const [totalPrice1, setTotalPrice1] = useState(price);
-  const [totalCard,setTotalCard] = useState(totalPrice)
+  const [totalCard, setTotalCard] = useState(totalPrice)
   const handleDelete = () => {
     onDelete(id);
   };
-  
+
   useEffect(() => {
     setTotalPrice1(count * price);
   }, [count, price]);
@@ -27,7 +27,7 @@ export default function CardItem({id,setTotalPrice, name,quantity,price,color,im
     const storedQuantity = currentItem ? currentItem.quantity : editQuantity;
     setEditQuantity(storedQuantity);
   }, [id, editQuantity]);
-  
+
   const handleIncrement = () => {
     setEditQuantity(prev => {
       const newQuantity = prev + 1;
@@ -35,11 +35,11 @@ export default function CardItem({id,setTotalPrice, name,quantity,price,color,im
       updateQuantityInLocalStorage(newQuantity);
       const currentItems = JSON.parse(localStorage.getItem("cart")) || [];
       const totalPriceAll2 = calculateTotalPrice(currentItems);
-    setTotalPrice(totalPriceAll2);
+      setTotalPrice(totalPriceAll2);
       return newQuantity;
     });
   };
-  
+
   const handleDecrement = () => {
     if (count > 1) {
       setEditQuantity(prev => {
@@ -48,20 +48,20 @@ export default function CardItem({id,setTotalPrice, name,quantity,price,color,im
         updateQuantityInLocalStorage(newQuantity);
         const currentItems = JSON.parse(localStorage.getItem("cart")) || [];
         const totalPriceAll2 = calculateTotalPrice(currentItems);
-      setTotalPrice(totalPriceAll2);
+        setTotalPrice(totalPriceAll2);
         return newQuantity;
       });
-    }else{
-      setCount(1);
+    } else {
+      setCount(0);
     }
   };
-  
+
   const updateQuantityInLocalStorage = (newQuantity) => {
     const storedCart = JSON.parse(localStorage.getItem("cart")) || [];
     const updatedCart = storedCart.map(item => {
       if (item.id === id) {
         const updatedItem = { ...item, quantity: newQuantity };
-        updatedItem.totalPrice = updatedItem.quantity * updatedItem.price; 
+        updatedItem.totalPrice = updatedItem.quantity * updatedItem.price;
         return updatedItem;
       }
       return item;
@@ -76,6 +76,9 @@ export default function CardItem({id,setTotalPrice, name,quantity,price,color,im
       setCount(newCount);
       setTotalPrice1(newCount * price);
       updateQuantityInLocalStorage(newCount);
+      const currentItems = JSON.parse(localStorage.getItem("cart")) || [];
+      const totalPriceAll2 = calculateTotalPrice(currentItems);
+      setTotalPrice(totalPriceAll2);
     }
   };
 
@@ -87,7 +90,7 @@ export default function CardItem({id,setTotalPrice, name,quantity,price,color,im
             <div className={styles.color} style={{
               backgroundColor: color
             }}></div>
-            <img className={styles.img} src={image}/>
+            <img className={styles.img} src={image} />
 
           </div>
           <p className={styles.title}>
@@ -99,27 +102,31 @@ export default function CardItem({id,setTotalPrice, name,quantity,price,color,im
         </div>
         <div className={styles.bottom}>
           <div className={styles.countContainer}>
-          <IconButton  onClick={ count > 0 ? handleDecrement : null}><RemoveIcon /> </IconButton>
-            <input className={`${styles.input}`} type='number'
-             min={1} 
-             max={100} 
-             value={count === 0 ?  '': count}
-             onChange={handleQuantityChange}/>
+            <IconButton onClick={count > 0 ? handleDecrement : null}><RemoveIcon /> </IconButton>
+            <input className={`${styles.input}`}
+              type='number'
+              min={'0'}
+              max={'100'}
+              maxLength={'3'}
+              value={count === 0 ? '' : count}
+              onChange={handleQuantityChange}
+            
+            />
             <IconButton onClick={handleIncrement}><AddIcon /> </IconButton>
           </div>
           <p className={styles.price}>
-          ${totalPrice1.toFixed(2)}
+            ${totalPrice1.toFixed(2)}
           </p>
-          <IconButton 
-          onClick={handleDelete}
-          sx={{
-            ":hover .MuiSvgIcon-root": {
-              color: 'red',
-            },
-            ":hover .MuiIconButton-root": {
-              bgcolor: 'transparent'
-            }
-          }}>
+          <IconButton
+            onClick={handleDelete}
+            sx={{
+              ":hover .MuiSvgIcon-root": {
+                color: 'red',
+              },
+              ":hover .MuiIconButton-root": {
+                bgcolor: 'transparent'
+              }
+            }}>
             <DeleteOutlineIcon sx={{
               color: '#C86823',
 
