9a3314d66b283493ebab53dd730ab9ac18b53df9
whathow <marwa@gmail.com>
2024-02-01 09:14:38 +0200

    fix user dashboard

--- src/Pages/DashUsers/DashUsers.js
+++ src/Pages/DashUsers/DashUsers.js
@@ -1,60 +1,146 @@
-// import Table from "../../Components/Table/Table";
-// import { faker } from "@faker-js/faker";
+import { Box, Button, Typography } from "@mui/material";
+import { useState } from "react";
+import { Helmet } from "react-helmet";
+import Table from "../../Components/Table/Table";
+import { useQuery } from "@tanstack/react-query";
+import axiosInstance from "../../Utils/AxiosInstance";
+import AddIcon from "@mui/icons-material/Add";
+import { ToastContainer } from "react-toastify";
+import UserModal from "../../Components/UserModal/UserModal";
+import DeleteUserModal from "../../Components/UserModal/DeleteModal";
 
 const DashUser = () => {
-//   const generateMockUsers = () => {
-//     const mockUsers = [];
+  const [selectedRowData, setSelectedRowData] = useState({});
+  const [openAdd, setOpenAdd] = useState(false);
+  const [openEdit, setOpenEdit] = useState(false);
+  const [openDelete, setOpenDelete] = useState(false);
 
-//     for (let i = 0; i < 20; i++) {
-//       const user = {
-//         _id: i + 1,
-//         firstName: faker.person.firstName(),
-//         lastName: faker.person.lastName(),
-//         role: i % 2 === 0 ? "Admin" : "Customer",
-//         email: faker.internet.email(),
-//         password: faker.internet.password(8),
-//         phoneNumber: faker.phone.phoneNumber(),
-//         image: faker.image.avatar(),
-//         createdAt: faker.date.past(),
-//         updatedAt: faker.date.recent(),
-//       };
-
-//       mockUsers.push(user);
-//     }
-//     console.log(mockUsers);
-
-//     return mockUsers;
-//   };
-
-//   const mockUsersWithIds = generateMockUsers();
+  const {
+    isPending: isUserPending,
+    isError: isUserError,
+    data: userData,
+    refetch: refetchUsers,
+  } = useQuery({
+    queryKey: ["userData"],
+    queryFn: async () => {
+      try {
+        const response = await axiosInstance.get(
+          `${process.env.REACT_APP_BACKEND_ENDPOINT}user`
+        );
+        return response.data;
+      } catch (error) {
+        console.error("Error fetching Users:", error);
+        throw error;
+      }
+    },
+  });
 
   return (
-    <>
-     {/* <Table 
-        isEdit={true}
-        ForWhat='users'
-        data={mockUsersWithIds}
-      />
-      <div>
-        <h2>User List</h2>
-        <ul>
-          {mockUsersWithIds.map((user) => (
-            <li key={user._id}>
-              <p>ID: {user._id}</p>
-              <p>First Name: {user.firstName}</p>
-              <p>Last Name: {user.lastName}</p>
-              <p>Email: {user.email}</p>
-              <p>Role: {user.role}</p>
-              <p>Phone Number: {user.phoneNumber}</p>
-              <p>Image: {user.image}</p>
-              <p>Created At: {user.createdAt.toString()}</p>
-              <p>Updated At: {user.updatedAt.toString()}</p>
-              <hr />
-            </li>
-          ))}
-        </ul>
-      </div>  */}
-    </>
+    <Box
+      sx={{ flexGrow: 1, display: "flex", flexDirection: "column", ml: "5rem" }}
+    >
+      <ToastContainer />
+      <Helmet>
+        <title>Manage Users </title>
+        <meta
+          name="description"
+          content="View a comprehensive overview of all hotels within Hotel Xpress's network. 
+          Access detailed information, performance metrics, and manage settings across 
+          multiple hotel properties in one convenient dashboard."
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta property="og:title" content="Manage Users" />
+        <meta
+          property="og:description"
+          content="View a comprehensive overview of all hotels within Hotel Xpress's network. 
+          Access detailed information, performance metrics, and manage settings across 
+          multiple hotel properties in one convenient dashboard."
+        />
+      </Helmet>
+      <Typography
+        variant="h4"
+        sx={{
+          textAlign: "left",
+          mb: 5,
+          mt: "2rem",
+          fontWeight: "bold",
+        }}
+      >
+        Manage Users
+      </Typography>
+      {isUserPending ? (
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Typography variant="h5" fontFamily="Helvetica Neue">
+            Loading...
+          </Typography>
+        </div>
+      ) : isUserError ? (
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Typography variant="h5" color="error">
+            Error loading data
+          </Typography>
+        </div>
+      ) : (
+        <>
+          <Button
+            onClick={() => setOpenAdd(true)}
+            endIcon={<AddIcon />}
+            variant="contained"
+            sx={{
+              width: "8rem",
+              bgcolor: "#C86823",
+              transition: "background-color 0.3s ease, color 0.3s ease",
+              textTransform: "none",
+              "&:hover": {
+                bgcolor: "#A0471D",
+                color: "white",
+              },
+            }}
+          >
+            Add User 
+          </Button>
+          <Table
+            data={userData && userData}
+            isEdit={true}
+            setSelectedRowData={setSelectedRowData}
+            handleOpenDelete={() => setOpenDelete(true)}
+            handleEditOpen={() => setOpenEdit(true)}
+            ForWhat={"users"}
+          />
+          <DeleteUserModal
+            open={openDelete}
+            handleClose={() => setOpenDelete(false)}
+            selectedRowData={selectedRowData && selectedRowData}
+            refetch={() => refetchUsers()}
+          />
+          <UserModal
+            open={openEdit}
+            handleClose={() => setOpenEdit(false)}
+            selectedRowData={selectedRowData}
+            refetch={() => refetchUsers()}
+            action={"edit"}
+          />
+          <UserModal
+            open={openAdd}
+            handleClose={() => setOpenAdd(false)}
+            refetch={() => refetchUsers()}
+            action={"add"}
+          />
+        </>
+      )}
+    </Box>
   );
 };
 
